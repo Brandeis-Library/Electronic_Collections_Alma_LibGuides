605e0af8f8f03fd4d2b4750e906062534d394b65
Chris Underwood <cunderwoodmn@gmail.com>
2020-04-30 16:59:15 -0400

    built first route to collections app to retreive 1st 10 collections using env variables

--- backend/routes/alma.js
+++ backend/routes/alma.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/collections', async function (req, res, next) {
+  try {
+
+    const { data } = await axios(process.env.EXLIBRIS_API_ROOT + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_PATH + `?limit=10&offset=0&apikey=` + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_KEY);
+    res.send(data);
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
