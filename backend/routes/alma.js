994c3e0a5cb46c55da5275c939aeaf344d1575c8
Chris Underwood <cunderwoodmn@gmail.com>
2020-04-30 18:25:23 -0400

    created route for finding an individual collection

--- backend/routes/alma.js
+++ backend/routes/alma.js
@@ -5,11 +5,22 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-/* GET users listing. */
+/* GET list of collections. */
 router.get('/collections', async function (req, res, next) {
   try {
 
     const { data } = await axios(process.env.EXLIBRIS_API_ROOT + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_PATH + `?limit=10&offset=0&apikey=` + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_KEY);
+    res.send(data);
+  } catch (error) {
+    next(error)
+  }
+});
+
+/* GET a single collection. */
+router.get('/collection/:collection_id', async function (req, res, next) {
+  try {
+    const { collection_id } = req.params
+    const { data } = await axios(process.env.EXLIBRIS_API_ROOT + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_PATH + `?` + collection_id + `&apikey=` + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_KEY);
     res.send(data);
   } catch (error) {
     next(error)
