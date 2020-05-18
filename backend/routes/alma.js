de49e9991a8b024b57dd8a7e3b592b3d4e41fc32
Chris Underwood <cunderwoodmn@gmail.com>
2020-05-18 16:32:04 -0400

    fixed minor issues in readme and fixed the individual collection route in alma to retreive one item only.

--- backend/routes/alma.js
+++ backend/routes/alma.js
@@ -4,14 +4,18 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 /* GET list of collections. */
 router.get('/collections', async function (req, res, next) {
   try {
-    const { data } = await axios(process.env.EXLIBRIS_API_ROOT + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_PATH + `?limit=10&offset=0&apikey=` + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_KEY);
+    const { data } = await axios(
+      process.env.EXLIBRIS_API_ROOT +
+        process.env.EXLIBRIS_ALMA_COLLECTIONS_API_PATH +
+        `?limit=10&offset=0&apikey=` +
+        process.env.EXLIBRIS_ALMA_COLLECTIONS_API_KEY
+    );
     res.send(data);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
@@ -19,10 +23,17 @@ router.get('/collections', async function (req, res, next) {
 router.get('/collection/:collection_id', async function (req, res, next) {
   try {
     const { collection_id } = req.params;
-    const { data } = await axios(process.env.EXLIBRIS_API_ROOT + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_PATH + `?` + collection_id + `&apikey=` + process.env.EXLIBRIS_ALMA_COLLECTIONS_API_KEY);
+    const { data } = await axios(
+      process.env.EXLIBRIS_API_ROOT +
+        process.env.EXLIBRIS_ALMA_COLLECTIONS_API_PATH +
+        `\\` +
+        collection_id +
+        `?apikey=` +
+        process.env.EXLIBRIS_ALMA_COLLECTIONS_API_KEY
+    );
     res.send(data);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
