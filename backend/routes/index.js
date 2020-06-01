48f3a9ba94a224e364e19561e3c9c55425b02e6b
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-01 18:46:57 -0400

    have a clean xml document from anies in index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const fs = require('fs');
+const express = require('express');
+const router = express.Router();
+
+const parser = require('xml2json');
+
+let doc = '';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  fs.readFile('anies.txt', 'utf8', async function (err, data) {
+    try {
+      console.log('inside readfile.');
+
+      data = data.replace(/\\/g, '');
+      console.log('data=======  ', data);
+      doc = data;
+      //JSON.parse(parser.toJson(data, { reversible: true }));
+      //console.log('data2+++++++++  ', data2);
+      //return data2;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  //json = await json.replace(/\\/g, '++++');
+  console.log('doc', doc);
+  res.render('index', { title: 'Express', txt: doc });
 });
 
 module.exports = router;
