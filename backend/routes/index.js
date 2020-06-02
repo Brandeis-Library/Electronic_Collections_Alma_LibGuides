3fca0192f9b1bb5bf90d1bd7ea91cc88d1a2b4e7
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-02 15:42:12 -0400

    remove one and add one npm package related to pulling text out of xml. Figured out how to pull the text for a specfic xml tag from a text file.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,33 +1,39 @@
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
-
-const parser = require('xml2json');
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
 
 let doc = '';
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  fs.readFile('anies.txt', 'utf8', async function (err, data) {
+  fs.readFile('anies.txt', 'utf8', async function (err, document) {
     try {
       console.log('inside readfile.');
 
-      data = data.replace(/\[\"/g, '"');
-      data = data.replace(/\"]/g, '"');
-      data = data.replace(/\\/g, '');
-      console.log('data=======  ', data);
-      doc = data;
-      //JSON.parse(parser.toJson(data, { reversible: true }));
-      //console.log('data2+++++++++  ', data2);
-      //return data2;
+      document = document.replace(/\[\"/g, '"');
+      document = document.replace(/\"]/g, '"');
+      document = document.replace(/\\/g, '');
+      console.log('document=======  ', document);
+      let xmlParsedDoc = new dom().parseFromString(document);
+      //console.log('xmlParsedDoc', xmlParsedDoc);
+      let nodes = xpath.select('//datafield[@tag=906]/subfield', xmlParsedDoc);
+      let nodesData = nodes[0].toString();
+      console.log('nodes=====  ', nodesData);
+      nodesData = nodesData.replace(/<subfield(.*?)>/g, '');
+      nodesData = nodesData.replace(/<\/subfield>/g, '');
+      console.log('nodes=====  ', nodesData);
+      doc = nodesData;
+
+      return res.render('index', { title: 'Express', txt: doc });
     } catch (err) {
       console.log(err);
     }
   });
 
-  //json = await json.replace(/\\/g, '++++');
-  console.log('doc', doc);
-  res.render('index', { title: 'Express', txt: doc });
+  //console.log('doc', doc);
+  //res.render('index', { title: 'Express', txt: doc });
 });
 
 module.exports = router;
